1871c506cc80970e314f9966dd71119f1436c087
Keith Cirkel <github@keithcirkel.co.uk>
2014-03-23 21:23:18 +0000

    Fix #5: Rename K-Class to KayClass

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*
- * # K-Class
+ * # KayClass
  *
  * > Copyright © 2013 Kay Framework Team
- * > K-Doc may be freely distributed under the MIT license.
+ * > KayClass may be freely distributed under the MIT license.
  * > For all details and documentation:
- * > http://github.com/kayframework/k-doc
+ * > http://github.com/kayframework/kayClass
  *
- * K-Class is a simple Class implementation inspired by Backbone, but using
+ * KayClass is a simple Class implementation inspired by Backbone, but using
  * ES5 features. This means it copies property values such as enumerability and
  * writability across classes. It also has proper prototypal inheritence using
  * Object.create.
@@ -16,7 +16,7 @@
  * a port of Node's EventEmitter, giving you the same flexibility on the
  * frontend, as in the backend.
  *
- * It is more comprehensive than a simple `util.inherits()` in Node, as it
+ * It is more comprehensive than a simple `util.inherits()` in Node.JS, as it
  * allows you to cleanly express additional prototype & static properties, and
  * also inherits static properties from the parent class. You also get a `super`
  * static property on your class, which references the parent, allowing you to
@@ -25,7 +25,7 @@
  * ### Usage
  *
  * Note:
- * When running in Node.JS, K-Class extends from
+ * When running in Node.JS, KayClass extends from
  * [Node.JS' Eventemitter](http://nodejs.org/api/events.html), when in the
  * browser, it has a functionality identical browser port of EventEmitter, see
  * [eventemitter-browser.js](./eventemitter-browser.js).
@@ -39,7 +39,7 @@
         getPropDesc = Object.getOwnPropertyDescriptor,
         global = (typeof window !== 'undefined' ? window : global),
         EventEmitter,
-        KClass;
+        KayClass;
 
     // Try to include EventEmitter the Node.JS way, falling back to taking it
     // from the global scope.
@@ -60,7 +60,7 @@
             i;
         for (i = 0; i < props.length; ++i) {
             replace = getPropDesc(onto, props[i]);
-            if (!(props[i] in Function) && (!replace || replace.writable)) {
+            if (!replace || replace.writable && replace.configurable) {
                 prop(onto, props[i], getPropDesc(from, props[i]));
             }
         }
@@ -110,17 +110,17 @@
         return child;
     }
 
-    KClass = extend(EventEmitter, {
+    KayClass = extend(EventEmitter, {
         /*
-         * KClass
+         * KayClass
          *
-         * Public: The K-Class constructor. Does nothing by itself really,
+         * Public: The KayClass constructor. Does nothing by itself really,
          *         but use the static .extend() method to extend it to your
          *         hearts content.
          *
          * Examples
          *
-         *     var Class = require('k-class');
+         *     var Class = require('kayclass');
          *     AThing = Class.extend({
          *         someProtoProp: 1,
          *         anotherProtoProp: 'hi',
@@ -142,8 +142,8 @@
          *         AnotherThing.prototype.someProtoMethod); // true
          *
          */
-        constructor: function KClass() {
-            KClass.super.call(this);
+        constructor: function KayClass() {
+            KayClass.super.call(this);
         },
         off: function off(name, callback) {
             if (callback === undefined) {
@@ -153,18 +153,18 @@
             }
         }
     });
-    // Node.JS EventEmitter has some static properties that KClass shouldn't
-    delete KClass.EventEmitter;
-    delete KClass.listenerCount;
+    // Node.JS EventEmitter has some static properties that KayClass shouldn't
+    delete KayClass.EventEmitter;
+    delete KayClass.listenerCount;
 
-    // The `.extend` function is a non-enumerable static property on K-Class
+    // The `.extend` function is a non-enumerable static property on KayClass
     // which is the crux of the Class system. `.extend` always results in a new
     // child class, which is a parent of the class it is being called from.
-    prop(KClass, 'extend', {
+    prop(KayClass, 'extend', {
         configurable: true,
         writable: true,
         /*
-         * KClass.extend
+         * KayClass.extend
          *
          * Public: Creates a new Class which inherits from `this`, adding
          *         the properties from `protoProps` to the prototype, and the
@@ -176,7 +176,7 @@
          *
          * Examples
          *
-         *     Person = KClass.extend({ name: 'Bob' });
+         *     Person = KayClass.extend({ name: 'Bob' });
          *     (new Person).name // => 'Bob'
          *     OtherPerson = Person.extend({ name: 'Sue', age: 21 });
          *     (new OtherPerson).name // => 'Sue'
@@ -184,7 +184,7 @@
          *
          * Returns a new Class.
          */
-        value: function KClassExtend(protoProps, staticProps) {
+        value: function KayClassExtend(protoProps, staticProps) {
             return extend(this, protoProps, staticProps);
         },
     });
@@ -192,12 +192,12 @@
     // Exporting
     // ---------
 
-    // Export out K-Class into a module.exports module (for Node)
+    // Export out KayClass into a module.exports module (for Node)
     // or a property on the window object (for Browsers)
     if (typeof module !== 'undefined' && module.exports) {
-        module.exports = KClass;
+        module.exports = KayClass;
     } else {
-        global.KClass = KClass;
+        global.KayClass = KayClass;
     }
 
 })();
